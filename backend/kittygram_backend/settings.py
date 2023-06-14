266f7855ad8a9f3ef9a3dd10ad5c70d9d3361007
Владас Куодис <vlad250577@yandex.ru>
2023-06-14 21:01:57 +0300

    Add secrets .env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'application_secure_key')
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
